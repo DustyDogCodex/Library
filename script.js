5c32ad1f6420bf0cdbebbdeddfe2b29e0239528b
Varun Malaviya <varunmalaviya7@gmail.com>
2022-07-12 20:19:19 -0500

    finally fixed card display issue. its innerText = 'value' and not innerText(value). FML.

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ let myLibrary = [];
 const addBook = document.querySelector('.addBookButton')
 const addBookModal = document.querySelector('.addBookForm')
 const confirmBookAdd = document.querySelector('#confirmBookAdd')
-const bookTitle = document.querySelector('#bookName')
+/* const bookTitle = document.querySelector('#bookName')
 const author = document.querySelector('#authorName')
-const pages = document.querySelector('#pages')
+const pages = document.querySelector('#pages') */
 
 //variable for combining book info into an object and then pushing it into myLibrary array.
-let newBook
+/* let newBook */
 
 addBook.addEventListener('click', () => {
     //on clicking this, the addBookForm window should pop up
     addBookModal.classList.add('show')
 })
 
-//adds book to library once user confirms it. Need to add variables that can access the data from addBooform and pass it here.
+//adds book to library once user confirms addition. Need to add variables that can access the data from addBookform and pass it here.
 confirmBookAdd.addEventListener('click', () => {
     addBookModal.classList.remove('show')
-    newBook = new Book(bookTitle,author,pages,read)
+    /* newBook = new Book(bookTitle,author,pages,read) */
 })
 
 //class for passing added books to the myLibrary array.
 class Book {
-    constructor(bookTitle, author, pages, read) {
-        this.title = bookTitle;
+    constructor(title, author, pages, read) {
+        this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
@@ -54,16 +54,14 @@ class UI {
 
         const books = exampleBooks;
 
-        books.forEach((value) => UI.addBookToLibrary(value) )
+        books.forEach((value) => UI.addBookToLibrary(value))
     }
 
     static addBookToLibrary(book){
-        myLibrary.push(newBook)
         const cardArea = document.querySelector('.cardArea')
         const newCard = document.createElement('div')
-    
-        newCard.innerText(`${book.title},${book.author},${book.pages},${book.read}`)
         newCard.classList.add('newCard')
+        newCard.innerText = `${book.title},${book.author},${book.pages},${book.read}`
         cardArea.appendChild(newCard)
     }    
 }
