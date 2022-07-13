c2b9e431fb94388dbcdc40852064b5eb0101e0d7
Varun Malaviya <varunmalaviya7@gmail.com>
2022-07-12 20:19:19 -0500

    changed object constructors to classes. Added an array with book info to see how things work while building the app.

--- script.js
+++ script.js
@@ -18,28 +18,56 @@ addBook.addEventListener('click', () => {
 confirmBookAdd.addEventListener('click', () => {
     addBookModal.classList.remove('show')
     newBook = new Book(bookTitle,author,pages,read)
-    addBookToLibrary
 })
 
-//object constructor that will pass added books to the myLibrary array.
-function Book(bookTitle,author,pages,read){
-    this.title = bookTitle
-    this.author = author
-    this.pages = pages
-    this.read = read
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read}`
+//class for passing added books to the myLibrary array.
+class Book {
+    constructor(bookTitle, author, pages, read) {
+        this.title = bookTitle;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.info = function () {
+            return `${title} by ${author}, ${pages} pages, ${read}`;
+        };
     }
 }
 
-/* JS to dynamically add html elements to DOM after as new book has been added */
-function addBookToLibrary(){
-    myLibrary.push(newBook)
-    console.log(myLibrary)
-    const newCard = document.createElement('div')
-    const cardArea = document.querySelector('.cardArea')
-    newCard.innerText('this is some rando book info')
-    newCard.classList.add('newCard')
-    cardArea.innerHTML(newCard)    
+/* JS to dynamically add html elements to DOM after a new book has been added */
+class UI {
+    static displayBooks() {
+        /* hard coded books here so i can see how they show up and that JS methods are working as intended */
+        const exampleBooks = [
+            {
+                title: "Winnie The Pooh",
+                author: "A.A. Milne",
+                pages: 420,
+                read: 'yes' 
+            },
+            {
+                title: "Asterix and Obelisk",
+                author: "A.A. Milne",
+                pages: 69,
+                read: 'yes' 
+            }
+        ]
+
+        const books = exampleBooks;
+
+        books.forEach((value) => UI.addBookToLibrary(value) )
+    }
+
+    static addBookToLibrary(book){
+        myLibrary.push(newBook)
+        const cardArea = document.querySelector('.cardArea')
+        const newCard = document.createElement('div')
+    
+        newCard.innerText(`${book.title},${book.author},${book.pages},${book.read}`)
+        newCard.classList.add('newCard')
+        cardArea.appendChild(newCard)
+    }    
 }
+
+//JS for displaying books on DOM
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
