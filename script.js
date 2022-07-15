60f0413e7bdf59d77269a7348700f8ef48b94378
Varun Malaviya <varunmalaviya7@gmail.com>
2022-07-14 20:19:19 -0500

    added JS validation for for fields, fixed bugs, added methods for reseting fields after submission and allowing the user to delete book cards already on display.

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ class Book {
 
 /* JS to dynamically add html elements to DOM after a new book has been added */
 class UI {
+    
     static displayBooks() {
         /* hard coded books here so i can see how they show up and that JS methods are working as intended */
         const exampleBooks = [
@@ -54,6 +55,7 @@ class UI {
         books.forEach((value) => UI.addBookToLibrary(value))
     }
 
+    //method for displaying books as cards on screen
     static addBookToLibrary(book){
         const cardArea = document.querySelector('.cardArea')
         const newCard = document.createElement('div')
@@ -61,6 +63,21 @@ class UI {
         newCard.innerText = `${book.title},${book.author},${book.pages},${book.read}`
         cardArea.appendChild(newCard)
     }    
+
+    //method for clearing the fields after user submits information
+    static resetFields() {
+        document.querySelector('#bookName').value = ''
+        document.querySelector('#authorName').value = ''
+        document.querySelector('#pages').value = ''
+        //will insert value for read checkbox later
+    }
+
+    //method for removing books 
+    static removeBook(el) {
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove();
+        }
+    }
 }
 
 //JS for displaying books on DOM
@@ -68,20 +85,36 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks)
 
 //JS for adding the book to the library by instantiating our book class
 document.querySelector('.addBookForm').addEventListener('submit', (e) => {
+
+    //prevent default submit event from happening. right now the book card only flashes and then vanishes.
     e.preventDefault();
+
     //values from the form are stored in variables bookTitle, author and pages.
     //need to add read or not as a value here too
-
     const bookTitle = document.querySelector('#bookName').value
     const author = document.querySelector('#authorName').value
     const pages = document.querySelector('#pages').value
     const read = true //setting it to true atm. Will get its value when i figure out how.
     
-    //instantate book class
-    const book = new Book(bookTitle,author,pages,read)
+    //Validation of entered variables using JS
+    //will change value of read later
+    if(bookTitle === '' || author === '' || pages === '' || read === false){
+        alert('Please fill in all fields before submitting!')
+    } else {
+        //instantate book class
+        const book = new Book(bookTitle,author,pages,read)
 
-    //Add book card to DOM
-    UI.addBookToLibrary(book)
-    console.log(book)
+        //Add book card to DOM
+        UI.addBookToLibrary(book)
+        console.log(book)
+
+        //reset all form fields
+        UI.clearFields();
+    }
+})
+
+//JS for removing books
+document.querySelector('.cardArea').addEventListener('click', (e) => {
+    UI.removeBook(e.target)
 })
 
